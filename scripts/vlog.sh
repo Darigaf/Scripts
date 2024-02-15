41a095df0c2a39ee29bf5c5843e650f971a3e422
Darigaf <mateus.bernardo@protonmail.com>
2024-02-15 02:23:41 -0300

    Added script to making vlogs

---
+++ scripts/vlog.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+#Script that records webcam and microphone sound using ffmpeg
+
+DATE=$(date +%d_%m_%Y\ %H_%M_%S);
+OUTPUT_NAME="$DATE $2";
+VIDEO_DEVICE="$1";
+if [ -z "$1" ];
+then
+        printf "Usage: vlog.sh VIDEO_INPUT OUTPUT_FILENAME\n";
+        printf "You must provide the path of the video input and the fikename of the video output\n";
+        exit 5;
+fi
+if [ -z "$2" ];
+then
+        printf "Usage: vlog.sh VIDEO_INPUT OUTPUT_FILENAME\n";
+        printf "You must provide the path of the video input and the fikename of the video output\n";
+        exit 5;
+fi
+ffmpeg -f pulse -ac 2 -i default -f v4l2 -i "$VIDEO_DEVICE" -vcodec libx265 "$OUTPUT_NAME";
