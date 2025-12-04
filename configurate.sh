bb87e33fe47ddf867962e54858243ececa2c3431
Mateus <mateus>
2025-12-04 09:22:23 -0300

    changed configuration script

--- configurate.sh
+++ configurate.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 #Simple script that copies all of the scripts from this folder to $HOME/.local/bin/ and makes tem executable
+
+mkdir -p "$HOME"/.local/bin/
 cp ./scripts/* "$HOME"/.local/bin/
 chmod u+x "$HOME"/.local/bin/*
